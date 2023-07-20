b38f8e7e436f4fb88458df4b196b62a206593550
gerveloper <gerveloper@gmail.com>
2023-07-19 23:18:51 -0300

    added nademailer function

--- app.js
+++ app.js
@@ -27,7 +27,33 @@ app.get('/register', function (req, res) {
 
 app.post('/register', async function (req, res) {
 
-    
+    let testAccount = await nodemailer.createTestAccount();
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+          user: testAccount.user,
+          pass: testAccount.pass,
+        },
+      });
+
+
+      //async function main() {
+        
+        let info = await transporter.sendMail({
+          from: '"Backend practice project" <no-reply@example.com>',
+          to: "you@example.com, andyou2@example.com",
+          subject: "(This is a test) Your registration has been successful",
+          text: "I hope this email finds you well. Thank you for getting this far!",
+          html: "<b>I hope this email finds you well. Thank you for getting this far!</b>",
+        });
+
+
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
     res.send( req.body )
 })
 
