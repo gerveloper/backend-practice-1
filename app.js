e7e9bb5679d5fbd95c2cd70de514908c26a93c44
gerveloper <gerveloper@gmail.com>
2023-08-03 22:07:43 -0300

    added categoriesRouter

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const app = express()
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/backend_practice_1', {useNewUrlParser: true, useUnifiedTopology: true});
 const authRouter = require('./src/routes/auth')
-
+const categoriesRouter = require('./src/routes/categories')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use('/auth', authRouter)
-
+app.use('/categories', categoriesRouter)
 
 app.get('/', function (req, res) {
     res.send('Bienvenido a backend')
