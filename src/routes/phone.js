22aaaf3555f68d407daf34235514699545e1e006
gerveloper <gerveloper@gmail.com>
2023-08-06 23:09:59 -0300

    added phone with basic routes

---
+++ src/routes/phone.js
@@ -0,0 +1,28 @@
+const router = require('express').Router()
+const Phone = require('../schemas/Phone')
+
+
+router.get('/', function () {
+
+    
+})
+
+
+router.get('/:id', function (req, res) {
+
+})
+
+
+router.post('/', function (req, res) {
+
+})
+
+
+router.patch('/:id', function (req, res) {
+
+})
+
+
+router.delete('/:id', function (req, res) {
+
+})
