7fdee0a9cbdb7b3f78e9c8f20eca53988afcc663
gerveloper <gerveloper@gmail.com>
2023-08-07 20:13:14 -0300

    added addresses router

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/backend_practice_1', {useNewUrlParser: true, useUnifiedTopology: true});
 const authRouter = require('./src/routes/auth')
 const categoriesRouter = require('./src/routes/categories')
+const addressesRouter = require('./src/routes/addresses')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use('/auth', authRouter)
 app.use('/categories', categoriesRouter)
+app.use('/addresses', addressesRouter)
+
 
 app.get('/', function (req, res) {
     res.send('Bienvenido a backend')
